73d150271f2b92c2f4608912d2e2333ff9630255
Jack Dubie <jdubie@cs.stanford.edu>
2012-05-13 01:28:34 -0700

    added vim, screen, and zsh lines

--- init.sh
+++ init.sh
@@ -5,4 +5,14 @@
 # sudo apt-get install vim
 # sudo apt-get install git-core
 
-# clone vim
+# setup screen
+git clone git@github.com:jdubie/screen.git ~/.screen
+cp ~/.screen/screenrc.screen-template ~/.screenrc
+
+# setup vim
+git clone git@github.com:jdubie/vim.git ~/.vim
+cp ~/.vim/vimrc.vim-template ~/.vimrc
+
+# setup zsh
+git clone git@github.com:jdubie/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
