d22fcaee86bf9fe257b7d604cabb0c552a66c6c2
Jack <jack@orchestra.com>
2012-05-30 14:04:17 -0700

    Now clones read-only repos. Should repo type as parameter in future

--- init.sh
+++ init.sh
@@ -6,13 +6,13 @@
 # sudo apt-get install git-core
 
 # setup screen
-git clone git@github.com:jdubie/screen.git ~/.screen
+git clone https://github.com/jdubie/screen.git ~/.screen
 cp ~/.screen/screenrc.screen-template ~/.screenrc
 
 # setup vim
-git clone git@github.com:jdubie/vim.git ~/.vim
+git clone https://github.com/jdubie/vim.git ~/.vim
 cp ~/.vim/vimrc.vim-template ~/.vimrc
 
 # setup zsh
-git clone git@github.com:jdubie/oh-my-zsh.git ~/.oh-my-zsh
+git clone https://github.com/jdubie/oh-my-zsh.git ~/.oh-my-zsh
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
