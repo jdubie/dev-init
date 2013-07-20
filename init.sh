5f60c5a630196bf7c98027285f49de1b993d794d
Jack Dubie <jdubie@dropbox.com>
2013-07-20 12:45:36 -0700

    Updated init script

--- init.sh
+++ init.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
 # install near and dear things
-sudo apt-get install -y zsh git-core vim tmux
+echo 'installing zsh, git, vim, tmux'
+sudo apt-get install -y zsh git-core vim tmux > /dev/null
 
 # setup vim
+echo 'configuring vim'
 git clone git@github.com:jdubie/vim ~/.vim
 ~/.vim/deploy.sh
 
 # setup tmux
-git clone git@github.com:jdubie/tmux
+echo 'configuring tmux'
+git clone git@github.com:jdubie/tmux ~/.tmux
+~/.tmux/deploy.sh
 cd ~
 ln -s ~/.tmux/tmux.conf .tmux.conf
 
 # setup zsh
+echo 'configuring zsh'
 git clone git@github.com:jdubie/oh-my-zsh ~/.oh-my-zsh
 ~/.oh-my-zsh/deploy.sh
 chsh -s `which zsh` # requires password
