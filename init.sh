2b98446a747139261159360bb4e213b1276edf83
Jack Dubie <jdubie@dropbox.com>
2013-07-20 13:01:52 -0700

    Quietly cloning repos

--- init.sh
+++ init.sh
@@ -6,15 +6,15 @@ sudo apt-get install -y zsh git-core vim tmux > /dev/null
 
 # setup vim
 echo 'configuring vim'
-git clone git@github.com:jdubie/vim ~/.vim > /dev/null
+git clone git@github.com:jdubie/vim ~/.vim --quiet
 ~/.vim/deploy.sh
 
 # setup tmux
 echo 'configuring tmux'
-git clone git@github.com:jdubie/tmux ~/.tmux > /dev/null
+git clone git@github.com:jdubie/tmux ~/.tmux --quiet
 ~/.tmux/deploy.sh
 
 # setup zsh
 echo 'configuring zsh'
-git clone git@github.com:jdubie/oh-my-zsh ~/.oh-my-zsh > /dev/null
+git clone git@github.com:jdubie/oh-my-zsh ~/.oh-my-zsh --quiet
 ~/.oh-my-zsh/deploy.sh
